d4e417ae0437fe2f68a43451117640b347cb4663
eugenmorok <64523467+eugenmorok@users.noreply.github.com>
2020-10-23 15:53:11 +0700

    Update ExchangeWithoutThirdVariable.java

--- ExchangeWithoutThirdVariable.java
+++ ExchangeWithoutThirdVariable.java
@@ -6,7 +6,7 @@ public class ExchangeWithoutThirdVariable {
         //your variables
         int first_variable = 0;
         int second_variable = 0;
-        int amount_variable = 0;
+        //int amount_variable = 0;
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello, I\'m the program and I want to take two numbers and swap them in the new way!");
@@ -15,13 +15,14 @@ public class ExchangeWithoutThirdVariable {
         System.out.print("Please, enter your second number: ");
         second_variable = in.nextInt();
 
-        amount_variable = first_variable + second_variable;
+        first_variable = first_variable + second_variable;
+        second_variable = first_variable - second_variable;
+        first_variable = first_variable -second_variable;
 
-        System.out.println("the first variable is " + (amount_variable - first_variable) + " now, and the second" +
-                " variable is " + (amount_variable - second_variable) + " now");
+        System.out.println("the first variable is " + (first_variable) + " now, and the second" +
+                " variable is " + (second_variable) + " now");
         System.out.println("the end of the program");
 
 
     }
 }
-
